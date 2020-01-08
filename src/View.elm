9706bb41a7be4cd7fe89c6ed7184d91e0e679959
Khirodkar <puja.khirodkar@gmail.com>
2020-01-08 19:15:55 +0900

    Refctoring for removing duplication of code with alignment of elements

--- src/View.elm
+++ src/View.elm
@@ -5,6 +5,7 @@ module View exposing (..)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border
+import Element.Font
 import Element.Input as Input exposing (button)
 import Html
 import Html.Attributes as Attributes exposing (align, height, src)
@@ -13,8 +14,16 @@ import Model
 import Msg
 
 
-
--- view : Model.Model -> List (Element.Element Msg.Msg)
+attr : List (Element.Attribute Msg.Msg)
+attr =
+    [ Element.height <| px 50
+    , Element.width <| px 100
+    , Background.color <| rgb 2.0 2.0 0.0
+    , Element.Border.rounded 5
+    , Element.Border.color <| rgb 1.0 1.0 1.0
+    , padding 10
+    , spacing 50
+    ]
 
 
 view : Model.Model -> Html.Html Msg.Msg
@@ -33,101 +42,53 @@ view model =
 
 view0 : Model.Model -> Element Msg.Msg
 view0 model =
-    let
-        attr =
-            [ Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
+    column [ centerX ]
+        [ column [ padding 10, width fill ]
+            [ Element.image [ centerX, Element.height <| px 500 ] { src = "flower1.png", description = "Zenia" } ]
+        , row [ centerX, spacing 20 ]
+            [ Input.button
+                attr
+                { onPress = Just Msg.Previous, label = text "Previous" }
+            , Input.button
+                attr
+                { onPress = Just Msg.Next, label = text "Next" }
+            , Input.button
+                attr
+                { onPress = Just Msg.First, label = text "First" }
+            , Input.button
+                attr
+                { onPress = Just Msg.Last, label = text "Last" }
             ]
-    in
-    column [ spacing 10, width fill ]
-        [ Element.image [ centerX ] { src = "flower1.png", description = "Zenia" }
-        , Input.button
-            [ alignLeft
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
-            ]
-            { onPress = Just Msg.Previous, label = text "Previous" }
-        , Input.button
-            [ centerX
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
-            ]
-            { onPress = Just Msg.Next, label = text "Next" }
-        , Input.button
-            [ alignLeft
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
-            ]
-            { onPress = Just Msg.First, label = text "First" }
-        , Input.button
-            [ alignRight
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
-            ]
-            { onPress = Just Msg.Last, label = text "Last" }
         ]
 
 
 view1 : Model.Model -> Element Msg.Msg
 view1 model =
-    column [ spacing 10, width fill ]
-        [ Element.image [ centerX ] { src = "flower2.png", description = "Rose" }
-        , Input.button
-            [ centerX
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
+    column [ centerX ]
+        [ column [ padding 10, width fill ]
+            [ Element.image [ centerX, Element.height <| px 500 ] { src = "flower2.png", description = "Rose" } ]
+        , row [ centerX, spacing 20 ]
+            [ Input.button
+                attr
+                { onPress = Just Msg.Next, label = text "Next" }
+            , Input.button
+                attr
+                { onPress = Just Msg.Last, label = text "Last" }
             ]
-            { onPress = Just Msg.Next, label = text "Next" }
-        , Input.button
-            [ alignRight
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
-            ]
-            { onPress = Just Msg.Last, label = text "Last" }
         ]
 
 
 view2 : Model.Model -> Element Msg.Msg
 view2 model =
-    column [ spacing 10, width fill ]
-        [ Element.image [ centerX ] { src = "flower3.png", description = "Lotus" }
-        , Input.button
-            [ alignLeft
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
+    column [ centerX ]
+        [ column [ padding 10, width fill ]
+            [ Element.image [ centerX, Element.height <| px 500 ] { src = "flower3.png", description = "Lotus" } ]
+        , row [ centerX, spacing 20 ]
+            [ Input.button
+                attr
+                { onPress = Just Msg.Previous, label = text "Previous" }
+            , Input.button
+                attr
+                { onPress = Just Msg.First, label = text "First" }
             ]
-            { onPress = Just Msg.Previous, label = text "Previous" }
-        , Input.button
-            [ alignRight
-            , Element.height <| px 50
-            , Element.width <| px 50
-            , Background.color <| rgb 2.0 5.0 0.0
-            , Element.Border.rounded 5
-            , Element.Border.color <| rgb 1.0 1.0 1.0
-            ]
-            { onPress = Just Msg.First, label = text "First" }
         ]
